58ae43316234b06d3776a196896083c5c3571d79
Monir <monirislam9498@gmail.com>
2025-09-22 05:43:31 +0600

    update

--- index.js
+++ index.js
@@ -4,7 +4,41 @@ let fullName = firstName + lastName;
 console.log(fullName)*/
 
 
-let num1 = 20;
+/*let num1 = 20;
 let num2 = 30;
 
-document.write ("num1=" + num1 + '<br/>' + "num2 = " + num2)
+document.write ("num1=" + num1 + '<br/>' + "num2 = " + num2)*/
+
+
+/*let text = prompt ("Enter your name");
+
+document.write(text.length)*/
+
+
+/*let text = "Bangladesh"
+
+document.write(text.charAt(3));*/
+
+/*let text = "Bangladesh";
+
+text = text.to();
+
+document.write(text)*/
+
+/*let text1 = " Bangladesh"
+let text2 = " is very beautiful"
+let text = text1.concat(text2)
+
+document.write(text)*/
+
+document.write('Personal Budget Tracker' + "<br/>")
+
+const userName = "Monirul Islam"
+const income = 30000;
+const expenses = 35000;
+
+
+
+document.write('User: '+  userName + "<br/>")
+document.write('Total Income: ' + '$' +  income + '<br/>')
+document.write('Total Expenses: ' + '$' + expenses)
