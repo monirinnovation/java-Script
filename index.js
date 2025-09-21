cb2da5f90052958badd127ee21ac2b519b5f1bf8
Monir <monirislam9498@gmail.com>
2025-09-21 20:10:58 +0600

    I learnt string concatinate

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("Hi")
+/*let firstName = "Monir"
+let lastName = "Islam";
+let fullName = firstName + lastName;
+console.log(fullName)*/
+
+
+let num1 = 20;
+let num2 = 30;
+
+document.write ("num1=" + num1 + '<br/>' + "num2 = " + num2)
