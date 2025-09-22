33adaa0e3e69d8dcc7612f5390387c600d4f7f3e
Monir <monirislam9498@gmail.com>
2025-09-22 12:26:36 +0600

    i learnt addition and subtraction if user open my site will see an input instructed to enter first number then second number as a rsult will get addition and subtraction

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ let text = text1.concat(text2)
 
 document.write(text)*/
 
-document.write('Personal Budget Tracker' + "<br/>")
+/*document.write('Personal Budget Tracker' + "<br/>")
 
 const userName = "Monirul Islam"
 const income = 30000;
@@ -39,6 +39,21 @@ const expenses = 35000;
 
 
 
-document.write('User: '+  userName + "<br/>")
-document.write('Total Income: ' + '$' +  income + '<br/>')
-document.write('Total Expenses: ' + '$' + expenses)
+document.write('User: '+  userName.toUpperCase() + "<br/>")
+document.write('Total Income: $' +  income + '<br/>')
+document.write('Total Expenses: $' + expenses)*/
+
+let num1 = Number(prompt("Enter your first number"));
+
+let num2 = Number(prompt("Enter your second number"));
+
+// num1 = parseInt(num1, 10)
+// num2 = parseInt(num2, 10)
+
+let sum, sub;
+
+sum = num1 + num2;
+sub = num1 - num2
+
+document.write("Addition = " + sum + "<br/>")
+document.write("Subtraction =" + sub)
